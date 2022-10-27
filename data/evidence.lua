5766831e5a00701e02bceda330b93eb0627fc3fa
nazdravi412 <81826403+nazdravi412@users.noreply.github.com>
2022-10-27 11:03:47 -0400

    Update evidence.lua
    Updated evidence locker to BCSO location.

--- data/evidence.lua
+++ data/evidence.lua
@@ -1,14 +1,14 @@
 return {
 	{
-		coords = vec3(458.97, -982.79, 30.68),
+		coords = vec3(1847.14, 3689.57, 34.27),
 		target = { -- qtarget support
-			name = 'mrpd_evidence', -- name of zone must be uniuqe
-			loc = vec3(459.07, -984.07, 30.69),
-			length = 1.4,
-			width = 3.2,
-			heading = 0,
-			minZ = 29.09,
-			maxZ = 31.89
+			name = 'bcso_evidence', -- name of zone must be uniuqe
+			loc = vec3(1847.75, 3689.95, 34.27),
+			length = 0.5,
+			width = 0.5,
+			heading = 300,
+			minZ = 31.47,
+			maxZ = 35.47
 		}
 	}
 }
